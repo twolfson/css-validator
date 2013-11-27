43fa0fe3f0793a9abd82d54bef23f8e12223c792
Todd Wolfson <todd@twolfson.com>
2013-11-27 00:32:49 -0800

    Completed initial test suite

--- test/css-validator_test.js
+++ test/css-validator_test.js
@@ -1,4 +1,17 @@
+var assert = require('assert');
+var fs = require('fs');
 var validateCss = require('../');
+
+function runValidateCss() {
+  before(function (done) {
+    var that = this;
+    validateCss(this.css, function (err, validationErrors) {
+      that.err = err;
+      that.validationErrors = validationErrors;
+      done();
+    });
+  });
+}
 
 describe('A valid CSS file', function () {
   before(function () {
@@ -6,18 +19,24 @@ describe('A valid CSS file', function () {
   });
 
   describe('when validated', function () {
-    // TODO: Move to common fn
-    before(function (done) {
-      var that = this;
-      validateCss(this.css, function (err, validationErrors) {
-        that.err = err;
-        that.validationErrors = validationErrors;
-        done();
-      });
-    });
+    runValidateCss();
 
     it('has no errors', function () {
       assert.strictEqual(validationErrors.length, 0);
+    });
+  });
+});
+
+describe('A invalid CSS file', function () {
+  before(function () {
+    this.css = fs.readFileSync(__dirname + '/test-files/invalid.css', 'utf8');
+  });
+
+  describe('when validated', function () {
+    runValidateCss();
+
+    it('has expected errors', function () {
+      assert.deepEqual(validationErrors, ['abc']);
     });
   });
 });
