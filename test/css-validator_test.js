4f1d5d9b66727cd9db7f1ee78b497f40c371aaa0
Todd Wolfson <todd@twolfson.com>
2013-11-27 23:26:58 -0800

    Corrected event name and everything is back to running

--- test/css-validator_test.js
+++ test/css-validator_test.js
@@ -6,6 +6,7 @@ function runValidateCss() {
   before(function (done) {
     var that = this;
     validateCss(this.css, function (err, data) {
+      console.log(err, data);
       that.err = err;
       that.data = data;
       done();
