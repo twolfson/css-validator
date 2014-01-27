082e4cd4308d4503d0ba4e8fb8f23603f9717a75
Todd Wolfson <todd@twolfson.com>
2014-01-26 23:30:35 -0800

    Removing dev console.log and timeout

--- test/css-validator_test.js
+++ test/css-validator_test.js
@@ -14,7 +14,6 @@ before(function () {
         'content-type': info.headers['content-type'].replace(/(\-+)\d+/, '$1somenumber')
       }, info.headers);
       info.body = info.body.replace(/(\-+)\d+/g, '$1somenumber');
-      console.log('hey', info);
     }
   })).listen(1337);
 });
@@ -25,7 +24,6 @@ after(function (done) {
 function runValidateCss() {
   before(function (done) {
     var that = this;
-    this.timeout(10000);
     validateCss({
       text: this.css,
       w3cUrl: 'http://localhost:1337/css-validator/validator'
