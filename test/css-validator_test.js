48460848c2f72994aa8ee9abac4de2e99fb838ad
Todd Wolfson <todd@twolfson.com>
2017-03-18 23:45:36 -0700

    Moved to easier to use test helper

--- test/css-validator_test.js
+++ test/css-validator_test.js
@@ -1,5 +1,4 @@
 // Load in our dependencies
-var assert = require('assert');
 var fs = require('fs');
 var expect = require('chai').expect;
 var extend = require('obj-extend');
@@ -7,14 +6,14 @@ var validateCss = require('../');
 var FakeJigsaw = require('./utils/fake-jigsaw');
 
 // Define our test helper
-function runValidateCss() {
+function runValidateCss(paramsFn) {
   FakeJigsaw.run();
   before(function (done) {
     var that = this;
-    assert(this.params);
+    var params = paramsFn();
     validateCss(extend({
       w3cUrl: 'http://localhost:1337/css-validator/validator'
-    }, this.params), function (err, data) {
+    }, params), function (err, data) {
       that.err = err;
       that.data = data;
       done();
@@ -23,127 +22,103 @@ function runValidateCss() {
 }
 
 // Define our tests
-describe('A valid CSS file', function () {
-  before(function () {
-    this.params = {
+describe('A valid CSS file being validated', function () {
+  runValidateCss(function () {
+    return {
       text: fs.readFileSync(__dirname + '/test-files/valid.css', 'utf8')
     };
   });
 
-  describe('when validated', function () {
-    runValidateCss();
-
-    it('has no errors', function () {
-      expect(this.data.validity).to.equal(true);
-      expect(this.data.errors).to.deep.equal([]);
-      expect(this.data.warnings).to.deep.equal([]);
-    });
+  it('has no errors', function () {
+    expect(this.data.validity).to.equal(true);
+    expect(this.data.errors).to.deep.equal([]);
+    expect(this.data.warnings).to.deep.equal([]);
   });
 });
 
-describe('A invalid CSS file', function () {
-  before(function () {
-    this.params = {
+describe('A invalid CSS file being validated', function () {
+  runValidateCss(function () {
+    return {
       text: fs.readFileSync(__dirname + '/test-files/invalid.css', 'utf8')
     };
   });
 
-  describe('when validated', function () {
-    runValidateCss();
+  it('was not valid errors', function () {
+    expect(this.data.validity).to.equal(false);
+  });
 
-    it('was not valid errors', function () {
-      expect(this.data.validity).to.equal(false);
-    });
+  it('has an expected error', function () {
+    var errors = this.data.errors;
+    expect(errors.length).to.equal(1);
+    expect(errors[0].message).to.contain('background-color');
+  });
 
-    it('has an expected error', function () {
-      var errors = this.data.errors;
-      expect(errors.length).to.equal(1);
-      expect(errors[0].message).to.contain('background-color');
-    });
-
-    it('has an expected warning', function () {
-      var warnings = this.data.warnings;
-      expect(warnings.length).to.equal(1);
-      expect(warnings[0].message).to.contain('-moz-box-sizing');
-    });
+  it('has an expected warning', function () {
+    var warnings = this.data.warnings;
+    expect(warnings.length).to.equal(1);
+    expect(warnings[0].message).to.contain('-moz-box-sizing');
   });
 });
 
-describe('A valid CSS URI', function () {
-  before(function () {
-    this.params = {
+describe('A valid CSS URI being validated', function () {
+  runValidateCss(function () {
+    return {
       uri: 'https://cdn.rawgit.com/twolfson/css-validator/0.7.0/test/test-files/valid.css'
     };
   });
 
-  describe('when validated', function () {
-    runValidateCss();
-
-    it('has no errors', function () {
-      expect(this.data.validity).to.equal(true);
-      expect(this.data.errors).to.deep.equal([]);
-      expect(this.data.warnings).to.deep.equal([]);
-    });
+  it('has no errors', function () {
+    expect(this.data.validity).to.equal(true);
+    expect(this.data.errors).to.deep.equal([]);
+    expect(this.data.warnings).to.deep.equal([]);
   });
 });
 
-describe('A invalid CSS URI', function () {
-  before(function () {
-    this.params = {
+describe('A invalid CSS URI being validated', function () {
+  runValidateCss(function () {
+    return {
       uri: 'https://cdn.rawgit.com/twolfson/css-validator/0.7.0/test/test-files/invalid.css'
     };
   });
 
-  describe('when validated', function () {
-    runValidateCss();
+  it('was not valid errors', function () {
+    expect(this.data.validity).to.equal(false);
+  });
 
-    it('was not valid errors', function () {
-      expect(this.data.validity).to.equal(false);
-    });
+  it('has an expected error', function () {
+    var errors = this.data.errors;
+    expect(errors.length).to.equal(1);
+    expect(errors[0].message).to.contain('background-color');
+  });
 
-    it('has an expected error', function () {
-      var errors = this.data.errors;
-      expect(errors.length).to.equal(1);
-      expect(errors[0].message).to.contain('background-color');
-    });
-
-    it('has an expected warning', function () {
-      var warnings = this.data.warnings;
-      expect(warnings.length).to.equal(1);
-      expect(warnings[0].message).to.contain('-moz-box-sizing');
-    });
+  it('has an expected warning', function () {
+    var warnings = this.data.warnings;
+    expect(warnings.length).to.equal(1);
+    expect(warnings[0].message).to.contain('-moz-box-sizing');
   });
 });
 
 // Edge cases
-describe('An empty CSS file', function () {
-  before(function () {
-    this.params = {text: ''};
+describe('An empty CSS file being validated', function () {
+  runValidateCss(function () {
+    return {text: ''};
   });
 
-  describe('when validated', function () {
-    runValidateCss();
-
-    it('has no errors', function () {
-      expect(this.data.validity).to.equal(true);
-      expect(this.data.errors).to.deep.equal([]);
-      expect(this.data.warnings).to.deep.equal([]);
-    });
+  it('has no errors', function () {
+    expect(this.data.validity).to.equal(true);
+    expect(this.data.errors).to.deep.equal([]);
+    expect(this.data.warnings).to.deep.equal([]);
   });
 });
 
-describe('A blank CSS file', function () {
-  before(function () {
-    this.params = {text: ' '};
+describe('A blank CSS file being validated', function () {
+  runValidateCss(function () {
+    return {text: ' '};
   });
 
-  describe('when validated', function () {
-    runValidateCss();
-
-    it('has no errors', function () {
-      expect(this.data.validity).to.equal(true);
-      expect(this.data.errors).to.deep.equal([]);
-      expect(this.data.warnings).to.deep.equal([]);
-    });
+  it('has no errors', function () {
+    expect(this.data.validity).to.equal(true);
+    expect(this.data.errors).to.deep.equal([]);
+    expect(this.data.warnings).to.deep.equal([]);
   });
 });
