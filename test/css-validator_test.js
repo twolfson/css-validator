1017caff0a63c54133506f1e443fb56028a80d6e
Todd Wolfson <todd@twolfson.com>
2017-03-18 23:42:42 -0700

    Added CSS URI testing but our text is persisting

--- test/css-validator_test.js
+++ test/css-validator_test.js
@@ -69,6 +69,52 @@ describe('A invalid CSS file', function () {
   });
 });
 
+describe('A valid CSS URI', function () {
+  before(function () {
+    this.params = {
+      uri: 'https://cdn.rawgit.com/twolfson/css-validator/0.7.0/test/test-files/valid.css'
+    };
+  });
+
+  describe('when validated', function () {
+    runValidateCss();
+
+    it('has no errors', function () {
+      expect(this.data.validity).to.equal(true);
+      expect(this.data.errors).to.deep.equal([]);
+      expect(this.data.warnings).to.deep.equal([]);
+    });
+  });
+});
+
+describe('A invalid CSS URI', function () {
+  before(function () {
+    this.params = {
+      uri: 'https://cdn.rawgit.com/twolfson/css-validator/0.7.0/test/test-files/invalid.css'
+    };
+  });
+
+  describe('when validated', function () {
+    runValidateCss();
+
+    it('was not valid errors', function () {
+      expect(this.data.validity).to.equal(false);
+    });
+
+    it('has an expected error', function () {
+      var errors = this.data.errors;
+      expect(errors.length).to.equal(1);
+      expect(errors[0].message).to.contain('background-color');
+    });
+
+    it('has an expected warning', function () {
+      var warnings = this.data.warnings;
+      expect(warnings.length).to.equal(1);
+      expect(warnings[0].message).to.contain('-moz-box-sizing');
+    });
+  });
+});
+
 // Edge cases
 describe('An empty CSS file', function () {
   before(function () {
